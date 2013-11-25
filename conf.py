9fb063e7ef183090f10de0c2dd429243d85e55b1
Andreas Tolfsen <ato@mozilla.com>
2013-11-25 21:21:57 +0000

    Theme placeholder

--- conf.py
+++ conf.py
@@ -6,11 +6,11 @@ import sys
 import os
 
 extensions = []
-templates_path = ["templates"]
 source_suffix = ".rst"
 master_doc = "index"
 project = u"Selenium"
 copyright = u"2013, Software Freedom Conservancy"
 exclude_patterns = ["_build", "authors.rst", "README.rst"]
-html_theme = "basic"
+html_theme = "selenium"
+html_theme_path = ["themes"]
 html_static_path = ["static"]
