74cbebec1edfd64bcab5f0eca42a4181154cd633
Andreas Tolfsen <ato@mozilla.com>
2014-12-29 20:21:34 +0100

    Pagination, footer, etc.

--- docs.js
+++ docs.js
@@ -11,6 +11,8 @@ window.onload = init;
 function init() {
 	addStructure();
 	addAnchors();
+	paginate();
+	insertFooter();
 }
 
 function addStructure() {}
@@ -28,4 +30,36 @@ function addAnchors() {
 	for (var h of hs) {
 		h.id = sanitise(h.textContent);
 	}
+}
+
+function paginate() {
+	var prev = document.querySelector("link[rel=prev]");
+	var next = document.querySelector("link[rel=next]");
+	var nav = document.createElement("nav");
+	nav.innerHTML = "<a class=prev href=" + prev.href + ">" + prev.title + "</a>" +
+		"<a class=next href=" + next.href + ">" + next.title + "</a>";
+	document.body.appendChild(nav);
+}
+
+// TODO(ato): Warning, this is unsafe.
+function insertFooter() {
+	var footer = document.createElement("footer");
+	var path = window.location.pathname;
+	if (path === "/docs" || path === "/docs/")
+		path = "/docs/index.html";
+	var file = path.substring(path.lastIndexOf("/") + 1);
+
+	footer.innerHTML = "<div class=links>" +
+		" </div>" +
+		"<div class=meta>" +
+		" <p>© 2013-2014, <a href=attr.html>Software Freedom Conservancy</a>." +
+		" <p>Last updated " + document.lastModified +
+		" <p>" +
+		"  <ul>" +
+		"   <li><a href=//github.com/seleniumhq/docs/issue>Report a bug</a>" +
+		"   <li><a href=//github.com/seleniumhq/docs/edit/gh-pages/" + file + ">Edit</a>" +
+		"  </ul>" +
+		"</div>";
+
+	document.body.appendChild(footer);
 }
